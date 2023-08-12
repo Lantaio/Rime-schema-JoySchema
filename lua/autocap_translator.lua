c2ac3b3584e27e0fcb8a0e67c009cfc5ca9872ec
Lantaio Joy <lantaio@yeah.net>
2023-08-12 17:58:37 +0800

    优化Lua文件格式。

--- lua/autocap_translator.lua
+++ lua/autocap_translator.lua
@@ -1,9 +1,9 @@
 -- 派生大写英文词汇
 local function autocap_translator(input, seg, env)
-  if string.match(input, '^%u[%l%d]+') then
-    local cand = Candidate("word_caps", seg.start, seg._end, string.upper(input), )
-    yield(cand)
-  end
+	if string.match(input, '^%u[%l%d]+') then
+		local cand = Candidate("word_caps", seg.start, seg._end, string.upper(input), )
+		yield(cand)
+	end
 end
 
 return autocap_translator
--- lua/is_in_user_dict.lua
+++ lua/is_in_user_dict.lua
@@ -5,24 +5,24 @@
 local M = {}
 
 function M.init(env)
-    local config = env.engine.schema.config
-    env.name_space = env.name_space:gsub('^*', '')
-    M.is_in_user_dict = config:get_bool(env.name_space)
+	local config = env.engine.schema.config
+	env.name_space = env.name_space:gsub('^*', '')
+	M.is_in_user_dict = config:get_bool(env.name_space)
 end
 
 function M.func(input, env)
-    for cand in input:iter() do
-        if M.is_in_user_dict then
-            if cand.type == "user_phrase" then
-                cand.comment = cand.comment .. '*'
-            end
-        else
-            if cand.type ~= "user_phrase" then
-                cand.comment = cand.comment .. '*'
-            end
-        end
-        yield(cand)
-    end
+	for cand in input:iter() do
+		if M.is_in_user_dict then
+			if cand.type == "user_phrase" then
+				cand.comment = cand.comment .. '*'
+			end
+		else
+			if cand.type ~= "user_phrase" then
+				cand.comment = cand.comment .. '*'
+			end
+		end
+		yield(cand)
+	end
 end
 
 return M
