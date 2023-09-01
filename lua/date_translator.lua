6affe199c391df380434ef7c1bae3beafcc08cb6
Lantaio Joy <lantaio@yeah.net>
2023-09-01 23:04:07 +0800

    优化日期候选项，删除前导0。

--- lua/date_translator.lua
+++ lua/date_translator.lua
@@ -23,11 +23,11 @@ function M.func(input, seg, env)
 	-- 日期
 	if (input == M.date) then
 		local current_time = os.time()
-		yield_cand(seg, os.date('%Y-%m-%d', current_time))
-		yield_cand(seg, os.date('%Y/%m/%d', current_time))
-		yield_cand(seg, os.date('%Y.%m.%d', current_time))
+		yield_cand(seg, os.date('%Y/%m/%d', current_time):gsub('/0', '/'))
+		yield_cand(seg, os.date('%Y年%m月%d日', current_time):gsub('年0', '年'):gsub('月0', '月'))
 		yield_cand(seg, os.date('%Y%m%d', current_time))
-		yield_cand(seg, os.date('%Y 年 %m 月 %d 日', current_time):gsub(' 0', ' '))
+		yield_cand(seg, os.date('%Y-%m-%d', current_time):gsub('-0', '-'))
+		yield_cand(seg, os.date('%Y.%m.%d', current_time):gsub('%.0', '.'))
 
 	-- 时间
 	elseif (input == M.time) then
