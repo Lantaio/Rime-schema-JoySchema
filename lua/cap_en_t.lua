62a1b46c4ad672986f53a552c5c7ba05f94a8e05
Lantaio Joy <lantaio@yeah.net>
2023-10-17 00:33:25 +0800

    删除不正确的程序。

--- lua/cap_en_t.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable-next-line: unused-local
-local function cap_en_t(input, seg, env)
-	if string.match(input, '^%u%u%l+') then
-		local cand = Candidate("word_caps", seg.start, seg._end, string.upper(input), '~AU')
-		yield(cand)
-	end
-end
-
-return cap_en_t
