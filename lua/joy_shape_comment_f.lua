c5675e4b29f4b27e238371480429aea4ff85c4e7
Lantaio Joy <lantaio@yeah.net>
2023-10-26 18:20:01 +0800

    修改全角、半角提示符，避免抢候选项眼球。

--- lua/joy_shape_comment_f.lua
+++ lua/joy_shape_comment_f.lua
@@ -1,9 +1,9 @@
 local function shape_comment_filter(input, env)
 	for cand in input:iter() do
 		if cand.comment:find("全角") then
-			cand.comment = cand.comment:gsub("〔全角〕", "🌕️")
+			cand.comment = cand.comment:gsub("〔全角〕", "●")
 		elseif cand.comment:find("半角") then
-			cand.comment = cand.comment:gsub("〔半角〕", "🌙️")
+			cand.comment = cand.comment:gsub("〔半角〕", "◗")
 		end
 		yield(cand)
 	end
