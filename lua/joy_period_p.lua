98482fc7bc0e6bc635cf27c75f91b911917b08a7
Lantaio Joy <lantaio@yeah.net>
2023-10-29 00:20:08 +0800

    继续优化处理逻辑，完善注释文档。

--- lua/joy_period_p.lua
+++ lua/joy_period_p.lua
@@ -6,30 +6,32 @@ local function period_processor(key, env)
 		-- 如果 没有候选框，就...
 		if not context:has_menu() then
 		-- 如果 按键是句点 并且 最近上屏的是数字，就...
-			if (key:repr() == 'period') and (tonumber(history:latest_text()) ~= nil)  then
+			if (key:repr() == 'period') and (tonumber(history:latest_text()) ~= nil) then
 				context.input = '.'
-				return 1  -- kAccepted 表示由本程序接收并处理按键事件。
+				return 1  -- kAccepted 表示仅由本程序处理此按键事件（此处为句点），已处理完毕。
 			end
 		-- 否则（有候选框）如果 待翻译串是句点，就...
 		elseif (context.input == '.') then
 			--  如果 按键是数字，就...
 			if (48 <= key.keycode and key.keycode <= 57) then
-				context:select(1)  -- 上屏英文句点
-				context:commit()
-				return 0  -- kRejected 表示将按键事件交回给系统继续处理
+				context:select(1)
+				context:commit()  -- 上屏第2个选项英文句点
+				return 0  -- kRejected 表示将按键事件（此处为数字）交回给系统继续处理。
 			-- 否则（按键不是数字）如果 按键是句点，就...
 			elseif (key:repr() == 'period') then
-				context:commit()  -- 上屏中文句号
+				context:commit()  -- 上屏默认选项中文句号
 				return 1
 			-- 否则（按键不是数字也不是句点）如果 按键是英文字母，就...
 			elseif (65 <= key.keycode and key.keycode <= 90) or (97 <= key.keycode and key.keycode <= 122) then
-				context:commit()  -- 上屏中文句号
-				env.engine:process_key(key)
-				return 1
+				context:commit()  -- 上屏默认选项中文句号
+				-- env.engine:process_key(key)
+				return 2  -- kNoop 表示将按键（此处为字母）交给下一个处理组件继续处理。
 			end
 		end
+	-- 否则（是ascii模式），就...
+	else
+		return 2  -- kNoop 表示将按键交给下一个处理组件继续处理。
 	end
-	return 2 -- kNoop 表示本程序不处理这个按键事件，交给下一个处理组件处理。
 end
 
 return period_processor
