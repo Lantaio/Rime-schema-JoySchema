64de997592304bd64768f521448e1a7802f472d5
Lantaio Joy <lantaio@yeah.net>
2023-10-28 18:29:22 +0800

    修正部分错误的判断条件。

--- lua/joy_period_p.lua
+++ lua/joy_period_p.lua
@@ -5,21 +5,25 @@ local function period_processor(key, env)
 	if not context:get_option("ascii_mode") then
 		-- 如果 没有候选框，就...
 		if not context:has_menu() then
-		-- 如果 按键是英文句点 并且 最近上屏的是数字，就...
-			if (key:repr():find('period')) and (tonumber(history:latest_text()) ~= nil)  then
+		-- 如果 按键是句点 并且 最近上屏的是数字，就...
+			if (key:repr() == 'period') and (tonumber(history:latest_text()) ~= nil)  then
 				context.input = '.'
 				return 1  -- kAccepted 表示由本程序接收并处理按键事件。
 			end
-		-- 否则（有候选框）如果 待翻译串是英文句号，就...
+		-- 否则（有候选框）如果 待翻译串是句点，就...
 		elseif (context.input == '.') then
 			--  如果 按键是数字，就...
 			if (48 <= key.keycode and key.keycode <= 57) then
-				context:select(1)  -- 上屏
+				context:select(1)  -- 上屏英文句点
 				context:commit()
 				return 0  -- kRejected 表示将按键事件交回给系统继续处理
-			-- 否则（按键不是数字）如果 按键是英文字母，就...
+			-- 否则（按键不是数字）如果 按键是句点，就...
+			elseif (key:repr() == 'period') then
+				context:commit()  -- 上屏中文句号
+				return 1
+			-- 否则（按键不是数字也不是句点）如果 按键是英文字母，就...
 			elseif (65 <= key.keycode and key.keycode <= 90) or (97 <= key.keycode and key.keycode <= 122) then
-				context:commit()  -- 上屏
+				context:commit()  -- 上屏中文句号
 				env.engine:process_key(key)
 				return 1
 			end
