49429d9f8c9272deb80fbb2fc9bc535a9db47855
Lantaio Joy <lantaio@yeah.net>
2023-10-30 01:37:01 +0800

    删除不需要的判断条件。

--- lua/joy_period_t.lua
+++ lua/joy_period_t.lua
@@ -1,8 +1,8 @@
 local function period_translator(input, seg, env)
 	local context = env.engine.context
 	local history = context.commit_history
-	-- 如果 不是ascii模式 并且 待翻译串是句点 并且 最近上屏的是数字，就...
-	if not context:get_option("ascii_mode") and input == '.' and tonumber(history:latest_text()) ~= nil then --
+	-- 如果 待翻译串是句点 并且 最近上屏的是数字，就...
+	if input == '.' and tonumber(history:latest_text()) ~= nil then
 		yield(Candidate(input, seg.start, seg._end, '。', '按句点or输拼音上屏中文句号'))
 		yield(Candidate(input, seg.start, seg._end, '.', '  选此项or输数字上屏英文句点'))
 	end
