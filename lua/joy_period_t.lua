dfb7067897bcea4111522f22bae87792863334e5
Lantaio Joy <lantaio@yeah.net>
2023-11-01 01:29:50 +0800

    修改函数名，优化判断条件。

--- lua/joy_period_t.lua
+++ lua/joy_period_t.lua
@@ -1,11 +1,11 @@
-local function period_translator(input, seg, env)
+local function joy_period_translator(input, seg, env)
 	local context = env.engine.context
 	local history = context.commit_history
 	-- 如果 待翻译串是句点 并且 最近上屏的是数字，就...
-	if input == '.' and tonumber(history:latest_text()) ~= nil then
+	if input == '.' and tonumber(history:latest_text()) then
 		yield(Candidate(input, seg.start, seg._end, '。', '按句点or输拼音上屏中文句号'))
 		yield(Candidate(input, seg.start, seg._end, '.', '  选此项or输数字上屏英文句点'))
 	end
 end
 
-return period_translator
+return joy_period_translator
