75a1e7d3834fd73d4a34a8e6ae56e539f2affdf9
Lantaio Joy <lantaio@yeah.net>
2023-11-01 14:05:14 +0800

    按空格键改为上屏英文句号+空格，优化注释。

--- lua/joy_period_p.lua
+++ lua/joy_period_p.lua
@@ -10,23 +10,23 @@ local function joy_period_processor(key, env)
 		end
 	-- 否则（有候选框）如果 待翻译串是句点，就...
 	elseif (context.input == '.') then
-		--  如果 按键是数字，就...
-		if 48 <= key.keycode and key.keycode <= 57 then
+		--  如果 按键是数字 或 空格键，就...
+		if 48 <= key.keycode and key.keycode <= 57 or key:repr() == 'space' then
 			context:select(1)
 			context:commit()  -- 上屏第2个候选项（英文句点）
 			return 0  -- kRejected 表示将按键事件（此处为数字）交回给系统继续处理。
-		-- 否则（按键不是数字）如果 按键是句点 或 空格键，就...
-		elseif key:repr() == 'period' or key:repr() == 'space' then
+		-- 否则（不是上面的按键）如果 按键是句点，就...
+		elseif key:repr() == 'period' then
 			context:select(0)
 			context:commit()  -- 上屏默认候选项（中文句号）
 			return 1
-		-- 否则（按键不是数字、句点和空格）如果 按键是英文字母，就...
+		-- 否则（不是上面的按键）如果 按键是英文字母，就...
 		elseif (65 <= key.keycode and key.keycode <= 90) or (97 <= key.keycode and key.keycode <= 122) then
 			context:select(0)
 			context:commit()  -- 上屏默认候选项（中文句号）
 			-- env.engine:process_key(key)
 			return 2  -- kNoop 表示将按键（此处为字母）交给下一个处理组件继续处理。
-		-- 否则（按键不是数字、句点、空格和英文字母）如果 按键是候选按键，就...
+		-- 否则（不是上面的按键）如果 按键是候选按键，就...
 		elseif key:repr() == 'comma' then  -- ！注意！如果不是用这些按键上屏候选项，须修改
 			context:select(1)
 			context:commit()  -- 上屏第2个候选项（英文句点）
