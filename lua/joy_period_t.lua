da3e410b1a132f2bc19db09dbdde6f9dddb49f5d
Lantaio Joy <lantaio@yeah.net>
2023-11-06 10:56:37 +0800

    优化提示语。

--- lua/joy_period_t.lua
+++ lua/joy_period_t.lua
@@ -3,8 +3,8 @@ local function joy_period_translator(input, seg, env)
 	local history = context.commit_history
 	-- 如果 待翻译串是句点 并且 最近上屏的是数字，就...
 	if input == '.' and tonumber(history:latest_text()) then
-		yield(Candidate(input, seg.start, seg._end, '。', '按句点or输拼音上屏中文句号'))
-		yield(Candidate(input, seg.start, seg._end, '.', '  选此项or输数字上屏英文句点'))
+		yield(Candidate(input, seg.start, seg._end, '。', '句号键or输拼音上屏中文句号'))
+		yield(Candidate(input, seg.start, seg._end, '.', '   逗号键or输数字上屏英文句点'))
 	end
 end
 
