37a76c01dbf5353b00a75dfd75a729ce3621ffa0
Lantaio Joy <lantaio@yeah.net>
2023-12-06 19:39:23 +0800

    调换欢乐句号功能候选项顺序。

--- lua/joy_period_p.lua
+++ lua/joy_period_p.lua
@@ -14,20 +14,20 @@ local function joy_period_processor(key, env)
 	elseif (context.input == '.') and not key:release() then
 		--  如果 按键是数字 或 空格键，就...
 		if tonumber(key:repr()) or key:repr() == 'space' then
-			context:select(1)  -- 上屏第2个候选项（小数点）
+			context:select(0)  -- 上屏第1个候选项（小数点）
 			return 0  -- kRejected 表示将按键事件（此处为数字）交回给系统继续处理。
 		-- 否则（不是上面的按键）如果 按键是句点，就...
 		elseif key:repr() == 'period' then
-			context:select(0)  -- 上屏第1个候选项（中文句号）
+			context:select(1)  -- 上屏第2个候选项（中文句号）
 			return 1
 		-- 否则（不是上面的按键）如果 按键是逗号键 或 回车键，就...
 		elseif key:repr() == 'comma' or key:repr() == 'Return' then
-			context:select(1)  -- 上屏第2个候选项（小数点）
+			context:select(0)  -- 上屏第1个候选项（小数点）
 			return 1
 		-- 否则（不是上面的按键）如果 按键是英文字母，就...
 		-- ！注意！keycode值为ASCII码值，而不是键盘的keycode值！
 		elseif (65 <= key.keycode and key.keycode <= 90) or (97 <= key.keycode and key.keycode <= 122) then
-			context:select(0)  -- 选定第1个候选项并上屏（中文句号）
+			context:select(1)  -- 选定第2个候选项并上屏（中文句号）
 			return 2  -- kNoop 表示将按键（此处为字母）交给下一个处理组件继续处理。
 		-- 否则（不是上面的按键）如果 按键是符号，就...
 		elseif 33 <= key.keycode and key.keycode <= 126 then
