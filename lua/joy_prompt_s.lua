a0e3a3c16be08f2227866bc8f105e41a8684e4dc
Lantaio Joy <lantaio@yeah.net>
2024-02-12 23:41:55 +0800

    🛠️增加全角模式判别及提示语。

--- lua/joy_prompt_s.lua
+++ lua/joy_prompt_s.lua
@@ -1,7 +1,7 @@
 --[[
-功能：本程序为触发特定规则的输入码添加提示语
+说明：本程序为触发特定规则的输入码添加提示语。
 作者：Lantaio Joy
-版本：2024.2.3
+版本：2024.2.11
 ]]--
 local function joy_prompt_segmentor(segs, env)
 	local context = env.engine.context
@@ -14,10 +14,11 @@ local function joy_prompt_segmentor(segs, env)
 		local Switch = {
 			[';'] = function()  seg.prompt = '☮扩展符号'  end,
 			['/'] = function()  seg.prompt = '🐧Linux/Mac目录路径'  end,
-			['~/'] = function()
-				seg = Segment(0, 2)
-				seg.prompt = '🐧Linux/Mac目录路径'
-			end,
+			['~/'] =
+				function()
+					seg = Segment(0, 2)
+					seg.prompt = '🐧Linux/Mac目录路径'
+				end,
 			['\\'] = function()  seg.prompt = '📁Win目录路径'  end,
 			['{'] = function()  seg.prompt = '🐱‍💻英文程序代码'  end,
 			['['] = function()  seg.prompt = '🐱‍💻英文程序代码'  end,
@@ -26,8 +27,13 @@ local function joy_prompt_segmentor(segs, env)
 			['$'] = function()  seg.prompt = '🀄中文数字金额'  end,
 		}
 		local case = Switch[segs.input]
-		if case then
+		-- 如果 不是全角模式 并且 待处理输入码片段在列表中 或者 待处理输入码片段是‘;’或者‘$’，就...
+		if (not context:get_option('full_shape') and case) or (segs.input == ';' or segs.input == '$') then
 			case()
+			segs:add_segment(seg)
+		-- 否则，如果是全角模式，就...
+		elseif context:get_option('full_shape') then
+			seg.prompt = '🌕全角模式'
 			segs:add_segment(seg)
 		end
 	end
