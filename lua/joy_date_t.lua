1bb2f0f9e278c139bd35a5af38ae2dc1a0eeb16c
Lantaio Joy <lantaio@yeah.net>
2024-03-13 00:49:01 +0800

    🐣️添加输入指定日期功能，✨️优化代码，添加候选词类型信息。

--- lua/joy_date_t.lua
+++ lua/joy_date_t.lua
@@ -1,8 +1,30 @@
--- 日期时间
-
+--[[
+说明：本程序使得在输入对应的拼音缩写时会生成日期、时间或星期等候选项。
+作者：Lantaio Joy
+版本：2024.3.5
+]]--
+local function isDate(year, month, day)
+	-- 如果年、月、日是有效数字，就...
+	if year and month and day then
+		-- 如果月份有效，就...
+		if month >= 1 and month <= 12 then
+			-- 判断日期是否在每月的有效天数范围内
+			local maxDaysOfMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+			-- 如果是闰年的2月，则天数为29
+			if month == 2 and (year % 4 == 0 and (year % 100 ~= 0 or year % 400 == 0)) then
+				maxDaysOfMonth[2] = 29
+			end
+			-- 如果日有效，就...
+			if day >= 1 and day <= maxDaysOfMonth[month] then
+				return true -- 返回日期有效
+			end
+		end
+	end
+	return false  -- 返回日期无效
+end
 -- 提高权重的原因：因为在方案中设置了大于 1 的 initial_quality，导致 rq sj xq dt ts 产出的候选项在所有词语的最后。
-local function yield_cand(seg, text)
-	local cand = Candidate('', seg.start, seg._end, text, '')
+local function yield_cand(type, seg, text)
+	local cand = Candidate(type, seg.start, seg._end, text, '')
 	cand.quality = 10
 	yield(cand)
 end
@@ -20,52 +42,50 @@ function M.init(env)
 end
 
 function M.func(input, seg, env)
-	-- 日期
+	-- 当前日期
 	if (input == M.date) then
-		local current_time = os.time()
-		yield_cand(seg, os.date('%Y/%m/%d', current_time):gsub('/0', '/'))
-		yield_cand(seg, os.date('%Y年%m月%d日', current_time):gsub('年0', '年'):gsub('月0', '月'))
-		yield_cand(seg, os.date('%Y%m%d', current_time))
-		yield_cand(seg, os.date('%Y.%m.%d', current_time):gsub('%.0', '.'))
-		yield_cand(seg, os.date('%Y-%m-%d', current_time):gsub('-0', '-'))
-
-	-- 时间
+		yield_cand('date', seg, os.date('%Y年%m月%d日'):gsub('年0', '年'):gsub('月0', '月'))
+		yield_cand('date', seg, os.date('%m月%d日'):gsub('^0', ''):gsub('月0', '月'))
+		yield_cand('date', seg, os.date('%Y/%m/%d'):gsub('/0', '/'))
+		yield_cand('date', seg, os.date('%Y%m%d'))
+		-- yield_cand('date', seg, os.date('%Y-%m-%d'):gsub('-0', '-'))
+		-- yield_cand('date', seg, os.date('%Y.%m.%d'):gsub('%.0', '.'))
+	-- 指定月日
+	elseif input:match('^rq%d%d%d%d$') then
+		month, day = input:match('^rq(%d%d)(%d%d)$')
+		month = tonumber(month)  day = tonumber(day)
+		if isDate(1976, month, day) then
+			yield_cand('date', seg, (month..'月'..day..'日'))
+		end
+	-- 指定年月日
+	elseif input:match('^rq%d%d%d%d%d%d%d%d$') then
+		year, month, day = input:match('^rq(%d%d%d%d)(%d%d)(%d%d)$')
+		year = tonumber(year)  month = tonumber(month)  day = tonumber(day)
+		if isDate(year, month, day) then
+			yield_cand('date', seg, (year..'年'..month..'月'..day..'日'))
+			yield_cand('date', seg, (year..'/'..month..'/'..day))
+			-- yield_cand('date', seg, (year..'-'..month..'-'..day))
+			-- yield_cand('date', seg, (year..'.'..month..'.'..day))
+		end
+	-- 当前时间
 	elseif (input == M.time) then
-		local current_time = os.time()
-		yield_cand(seg, os.date('%H:%M', current_time))
-		yield_cand(seg, os.date('%H:%M:%S', current_time))
-
-	-- 星期
+		yield_cand('time', seg, os.date('%H:%M'))
+		yield_cand('time', seg, os.date('%H:%M:%S'))
+	-- 当前星期
 	elseif (input == M.week) then
-		local current_time = os.time()
 		local week_tab = {'日', '一', '二', '三', '四', '五', '六'}
-		local text = week_tab[tonumber(os.date('%w', current_time) + 1)]
-		yield_cand(seg, '星期' .. text)
-		yield_cand(seg, '礼拜' .. text)
-		yield_cand(seg, '周' .. text)
-
+		local text = week_tab[tonumber(os.date('%w') + 1)]
+		yield_cand('week', seg, '星期' .. text)
+		-- yield_cand('week', seg, '礼拜' .. text)
+		yield_cand('week', seg, '周' .. text)
 	-- ISO 8601/RFC 3339 的时间格式 （固定东八区）（示例 2022-01-07T20:42:51+08:00）
 	elseif (input == M.datetime) then
-		local current_time = os.time()
-		yield_cand(seg, os.date('%Y-%m-%dT%H:%M:%S+08:00', current_time))
-		yield_cand(seg, os.date('%Y%m%d%H%M%S', current_time))
-
+		yield_cand('datetime', seg, os.date('%Y-%m-%dT%H:%M:%S+08:00'))
+		yield_cand('datetime', seg, os.date('%Y%m%d%H%M%S'))
 	-- 时间戳（十位数，到秒，示例 1650861664）
 	elseif (input == M.timestamp) then
-		local current_time = os.time()
-		yield_cand(seg, string.format('%d', current_time))
+		yield_cand('timestamp', seg, string.format('%d', os.time()))
 	end
-
-	-- -- 显示内存
-	-- local cand = Candidate("date", seg.start, seg._end, ("%.f"):format(collectgarbage('count')), "")
-	-- cand.quality = 100
-	-- yield(cand)
-	-- if input == "xxx" then
-	--     collectgarbage()
-	--     local cand = Candidate("date", seg.start, seg._end, "collectgarbage()", "")
-	--     cand.quality = 100
-	--     yield(cand)
-	-- end
 end
 
 return M
