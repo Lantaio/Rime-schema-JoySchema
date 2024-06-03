ff0f03a15a46b0afb3800acb329a1425ef087231
Lantaio Joy <lantaio@yeah.net>
2024-06-03 23:51:51 +0800

    🐱‍🏍优化Win目录路径识别规则，添加注释。

--- lua/joy_prompt_s.lua
+++ lua/joy_prompt_s.lua
@@ -1,7 +1,7 @@
 --[[
 说明：本程序为触发特定规则的输入码添加提示语。
 作者：Lantaio Joy
-版本：2.4.12
+版本：2.4.13
 更新：2024/6/3
 ]]--
 local function joy_prompt_segmentor(segs, env)
@@ -19,21 +19,23 @@ local function joy_prompt_segmentor(segs, env)
 	}
 	local first_char = input:sub(1, 1)
 	local switch = Cases[first_char]
-	-- 如果 不是全角模式 并且 输入码以Cases表中的标点符号开头 或者 输入码以‘^’或者‘$’开头，就...
+	-- 如果 不是全角模式 并且 输入码以Cases表中的标点符号开头 或者 输入码以‘^’或者‘$’开头，则……
 	if (not context:get_option('full_shape') and switch) or first_char == '^' or first_char == '$' then
 		switch()
 		segs:add_segment(seg)
-	-- 否则，如果是全角模式，就...
+	-- 否则，如果是全角模式，则……
 	elseif context:get_option('full_shape') then
 		seg.prompt = '🌕全角模式'
 		segs:add_segment(seg)
-	-- 否则，如果输入码以‘rq’开头，就...
+	-- 否则，如果输入码以‘rq’开头，则……
 	elseif input == 'rq' then
 		seg.prompt = '📆日期 示例：20210601'
 		segs:add_segment(seg)
-	elseif input:find('^%a:') or input:find('\\', 1, true) then
+	-- 否则，如果输入码以‘x:’ 或 ‘\’开头，则……
+	elseif input:find('^%a:') or input:sub(1, 1) == '\\' then
 		seg.prompt = '📁Win目录路径'
-		if input:find('[/*?"<>|]') or input:find(':', 3, true) or input:find('^\\:') then
+		-- 如果输入码包含[/*?"<>|] 或 从第3个字符起有‘:’号 或 以‘\:’开头，则显示警告信息
+		if input:find('[/*?"<>|]') or input:find(':', 3, true) or input:sub(1, 2) == '\\:' then
 			seg.prompt = '❌Win目录路径不能有/:*?"<>|'
 		end
 		segs:add_segment(seg)
