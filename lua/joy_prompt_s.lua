f610a9cbba771436da92f67bb780907db1fd3c7b
Lantaio Joy <lantaio@yeah.net>
2024-09-02 17:28:44 +0800

    ✨添加邮箱提示语。

--- lua/joy_prompt_s.lua
+++ lua/joy_prompt_s.lua
@@ -1,8 +1,8 @@
 --[[
 说明：本程序为触发特定规则的输入码添加提示语。
 作者：Lantaio Joy
-版本：2.4.15
-更新：2024/6/5
+版本：2.5.16
+更新：2024/8/30
 ]]--
 local function joy_prompt_segmentor(segs, env)
 	local context = env.engine.context
@@ -21,22 +21,24 @@ local function joy_prompt_segmentor(segs, env)
 	-- 如果 不是全角模式 并且 输入码以Cases表中的标点符号开头 或者 输入码以‘^’或者‘$’开头，则……
 	if (not context:get_option('full_shape') and switch) or first_char == '^' or first_char == '$' then
 		switch()
-		segs:add_segment(seg)
 	-- 否则，如果是全角模式，则……
 	elseif context:get_option('full_shape') then
 		seg.prompt = '🌕全角模式'
-		segs:add_segment(seg)
 	-- 否则，如果输入码以‘rq’开头，则……
 	elseif input == 'rq' then
-		seg.prompt = '📆日期 示例：20210601'
-		segs:add_segment(seg)
-	-- 否则，如果输入码以‘x:’ 或 ‘\’开头，则……
+		seg.prompt = '📆指定日期，例：rq19980605'
+	-- 否则，如果输入码以‘x:’ 或 其中有‘\’号，则……
 	elseif input:find('^%a:') or input:find('\\', 1, true) then
 		seg.prompt = '📁Win目录路径'
 		-- 如果输入码包含[/*?"<>|] 或 从第3个字符起有‘:’号 或 以‘\:’开头，则显示警告信息
 		if input:find('[/*?"<>|]') or input:find(':', 3, true) or input:sub(1, 2) == '\\:' then
 			seg.prompt = '❌Win目录路径不能有/:*?"<>|'
 		end
+	-- 否则，如果输入码从第2位开始有‘@’号，则……
+	elseif input:find('@', 2, true) then
+		seg.prompt = '📧email地址'
+	end
+	if seg.prompt then
 		segs:add_segment(seg)
 	end
 
